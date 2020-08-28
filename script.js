7f2d2a857cfa7b7513a2203d0910a55fd649585a
George W Langham <georgelangham1990@gmail.com>
2020-08-28 16:22:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+const messageEl = document.getElementById("message");
+
+const videoEl = document.getElementById("video");
+
+const dateNow = new Date();
+
+const day = dateNow.getDay();
+
+const isWednesday = day === 3;
+
+function getColours() {
+  const getColour = () => Math.ceil(Math.random() * 255);
+  const red = getColour();
+  const green = getColour();
+  const blue = getColour();
+  return { red, green, blue };
+}
+
+const { red, green, blue } = getColours();
+
+document.body.style.background = `rgba(${red}, ${green}, ${blue}, .1`;
+
+console.log(getColours());
+if (isWednesday) {
+  messageEl.innerText = "IT IS WEDNESDAY MY DUDES";
+} else {
+  messageEl.innerText = "NOPE";
+  videoEl.remove();
+}
